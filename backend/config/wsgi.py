c93895fcfc0662a959022bef3251013051ca2d5c
Segera <your.segeramos89@gmail.com>
2026-03-18 21:55:50 +0300

    fix wsgi.py conflict markers

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,8 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-<<<<<<< HEAD
-
-=======
->>>>>>> backend/setup
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 application = get_wsgi_application()
